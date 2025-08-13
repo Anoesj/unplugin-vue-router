084193966be3c81c3cd763b1c3352d74196d257a
Anoesj <anoesjsadraee@gmail.com>
2025-08-13 17:43:33 +0200

    feat: output `RouteRecordInfo` over multiple lines

--- src/codegen/generateRouteMap.ts
+++ src/codegen/generateRouteMap.ts
@@ -27,6 +27,8 @@ export function generateRouteRecordInfo(node: TreeNode) {
     generateRouteParams(node, false),
   ]
 
+  let childRouteNames = 'never'
+
   if (node.children.size > 0) {
     // TODO: remove Array.from() once Node 20 support is dropped
     const deepNamedChildren = Array.from(node.getChildrenDeep())
@@ -38,9 +40,13 @@ export function generateRouteRecordInfo(node: TreeNode) {
       .sort()
 
     if (deepNamedChildren.length > 0) {
-      typeParams.push(deepNamedChildren.join(' | '))
+      childRouteNames = deepNamedChildren
+        .map((childRouteName) => `| ${childRouteName}`)
+        .join('\n    ')
     }
   }
 
-  return `RouteRecordInfo<${typeParams.join(', ')}>`
+  typeParams.push(childRouteNames)
+
+  return `RouteRecordInfo<${typeParams.map((line) => `\n    ${line}`).join(',\n')}\n  >`
 }
